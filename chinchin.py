62eb07d9fd575a85393b3da132b8fe91c70213c6
Han <tyou.han847@mail.kyutech.jp>
2026-01-16 02:18:01 +0900

    osukechin

--- chinchin.py
+++ chinchin.py
@@ -1,6 +1,9 @@
 input ("Press Enter to continue...")
 print ("Welcome to Chinchin.py!")
 name = input("What is your name? ")
+if name == "osuke":
+    print("you are chinchin.")
+
 print(f"Hello, {name}! Let's play a game.")
 age = int(input("How old are you? "))
 if age < 18:
