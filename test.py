ca7bd94d2a7d5eecc4c86bf4d21e00136390aa91
Han <tyou.han847@mail.kyutech.jp>
2026-01-16 01:44:35 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+print("Thank you for using the program.")
+print("Goodbye!")
+
+    
