ab7baed5a383f5d3b3d03d1dd803c44eab4e1b15
Han1121-kyutech <tyou.han847@mail.kyutech.jp>
2026-01-16 13:52:19 +0900

    modify test.py

--- test.py
+++ test.py
@@ -6,7 +6,11 @@ if age < 18:
 else:
     print("You are an adult.")
 print("Thank you for using the program.")
-print("Next chinchin is you.")
+yN = input("Do you want to be a chinchin? [y/N]")
+if yN == "y":
+    print("Next chinchin is you.")
+else:
+    print("Next manman is you.")
 print("Goodbye!")
 print("Fuck you!")
 print("you're welcome!")    
