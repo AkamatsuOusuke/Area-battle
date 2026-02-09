1dcc2f8b4f6a9827311737c7a389f37c2b4456c2
AkamatsuOusuke <216109714+AkamatsuOusuke@users.noreply.github.com>
2026-02-10 01:41:36 +0900

    css削除

--- Test.py
+++ Test.py
@@ -5,3 +5,12 @@ if age < 18:
     print("You are a minor.")
 else:
     print("You are an adult.")
+print("Thank you for using the program.")
+yN = input("Do you want to be a chinchin? [y/N]")
+if yN == "y":
+    print("Next chinchin is you.")
+else:
+    print("Next manman is you.")
+print("Goodbye!")
+print("Fuck you!")
+print("you're welcome!")    
