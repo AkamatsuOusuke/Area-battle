a75da31addf907903b1898220ed23cacf8e55738
AkamatsuOusuke <216109714+AkamatsuOusuke@users.noreply.github.com>
2026-02-09 23:42:47 +0900

    2/9時点

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from fastapi import FastAPI
 # CORS（Cross-Origin Resource Sharing）を制御するためのミドルウェア。どこからでもアクセスOKにしてくれてるらしい
 from fastapi.middleware.cors import CORSMiddleware 
 
-
+# 図形操作ライブラリ
 from shapely.geometry import Polygon
+
+# 座標変換ライブラリ
+from pyproj import Transformer
 
 app = FastAPI() # サーバー本体を、appという名前で作成
 
@@ -14,6 +17,12 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+transformer = Transformer.from_crs(
+    "EPSG:4326",  # WGS84（緯度経度）
+    "EPSG:3857",  # Webメルカトル（平面直角座標系）
+    always_xy=True,
 )
 
 @app.post("/area") # /areaにPOSTリクエストが来たら、以下の関数を実行
