e2c606cb886cc64bdd185ce7057936c351a2d4f8
Han1121-kyutech <tyou.han847@mail.kyutech.jp>
2026-02-10 23:30:50 +0900

    ははんだよ

--- test.py
+++ test.py
@@ -5,12 +5,3 @@ if age < 18:
     print("You are a minor.")
 else:
     print("You are an adult.")
-print("Thank you for using the program.")
-yN = input("Do you want to be a chinchin? [y/N]")
-if yN == "y":
-    print("Next chinchin is you.")
-else:
-    print("Next manman is you.")
-print("Goodbye!")
-print("Fuck you!")
-print("you're welcome!")    
